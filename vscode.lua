7a58c0697881ff3f30d30bd86233725451d34634
rekitrelt <67936854+rekitrelt@users.noreply.github.com>
2024-08-12 02:59:46 -0700

    Add files via upload

---
+++ vscode.lua
@@ -0,0 +1,45 @@
+spawn(function()
+repeat task.wait() until game:IsLoaded()
+
+local lp = game.Players.LocalPlayer
+local SMethod = (WebSocket and WebSocket.connect)
+
+if not SMethod then return lp:Kick("Executor is too shitty.") end
+
+local Main = function()
+	WebSocket = SMethod("ws://localhost:9000/")
+    local Closed = false
+
+	if not WebSocket then return end
+
+	WebSocket:Send(game.HttpService:JSONEncode({
+		Method= "Authorization",
+		Name= lp.Name
+	}))
+
+	WebSocket.OnMessage:Connect(function(Unparsed)
+		local Parsed= game.HttpService:JSONDecode(Unparsed)
+
+		if (Parsed.Method == "Execute") then
+			local FFunction, Error= loadstring(Parsed.Data)
+
+			if Error then return WebSocket:Send(game.HttpService:JSONEncode({
+				Method= "Error",
+				Message= Error
+			}))end
+			spawn(FFunction)
+		end
+	end)
+
+	WebSocket.OnClose:Connect(function()
+			Closed= true
+		end)
+
+		repeat task.wait() until Closed
+	end
+	
+	while task.wait(1) do
+		local Success, Error = pcall(Main)
+		if not Success then print(Error) end
+	end
+end)
